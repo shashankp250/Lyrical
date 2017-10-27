33fc0f19708f9228005ee2f285ac5ebceb1433f7
Shashank <shashankpawar@Shashanks-MacBook-Air.local>
2017-10-28 04:30:36 +0530

    added react router, song detail, songs list, add new song component, delete song

---
+++ client/components/SongCreate.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+
+import query from '../queries/fetchSongs';
+
+class SongCreate extends Component {
+  constructor(props){
+    super(props);
+    this.state = {title : ''};
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+
+    this.props.mutate({
+      variables:{
+        title: this.state.title
+      },
+      refetchQueries:[{ query}]
+    }).then( () => hashHistory.push('/') )
+  }
+
+  render(){
+    return(
+      <div>
+      <Link to="/">Back</Link>
+        <h3>Create New Song</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title :</label>
+          <input value={this.state.title} onChange={event=>this.setState({title: event.target.value})}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mutation = gql `
+mutation AddSong($title:String){
+  addSong(title:$title){
+    title
+  }
+}
+`;
+
+export default graphql (mutation) (SongCreate);
